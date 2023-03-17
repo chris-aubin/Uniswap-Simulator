3a05432bb3c214fe6a19cbe432aa07781c65cd83
Chris Aubin <69503979+chris-aubin@users.noreply.github.com>
2023-03-17 15:52:33 -0400

    Tick update function

--- src/libraries/tick/tick.go
+++ src/libraries/tick/tick.go
@@ -3,6 +3,7 @@ package tick
 import (
 	"math/big"
 
+	"github.com/chris-aubin/Uniswap-Simulator/src/libraries/liquidityMath"
 	"github.com/chris-aubin/Uniswap-Simulator/src/libraries/tickMath"
 )
 
@@ -10,10 +11,7 @@ var (
 	MaxUint128, _ = new(big.Int).SetString("0xffffffffffffffffffffffffffffffff", 16)
 )
 
-type Ticks struct {
-	TickData map[int]*Tick
-}
-
+// Info stored for each initialized individual tick
 type Tick struct {
 	// The total position liquidity that references this tick
 	LiquidityGross *big.Int
@@ -30,10 +28,16 @@ type Tick struct {
 	SecondsPerLiquidityOutsideX128 *big.Int
 	// The seconds spent on the other side of the tick (relative to the current tick)
 	// Only has relative meaning, not absolute — the value depends on when the tick is initialized
-	SecondsOutside *big.Int
+	SecondsOutside int
 	// True iff the tick is initialized, i.e. the value is exactly equivalent to the expression liquidityGross != 0
 	// These 8 bits are set to prevent fresh sstores when crossing newly initialized ticks
 	Initialized bool
+}
+
+// Contains all all tick information for initialized ticks
+type Ticks struct {
+	// Maps tick index to tick data
+	TickData map[int]*Tick
 }
 
 // Derives max liquidity per tick from given tick spacing
@@ -96,6 +100,64 @@ func (t *Ticks) getFeeGrowthInside(
 	feeGrowthInside1X128.Sub(feeGrowthGlobal1X128, new(big.Int).Sub(feeGrowthBelow1X128, feeGrowthAbove1X128))
 
 	return feeGrowthInside0X128, feeGrowthInside1X128
+}
+
+// Updates a tick and returns true if the tick was flipped from initialized to uninitialized, or vice versa
+// Accepts tick, the index of the tick that will be updated
+// Accepts tickCurrent, the index of the current tick
+// Accepts liquidityDelta, the (new) amount of liquidity to be added (subtracted) when tick is crossed from left to right (right to left)
+// Accepts feeGrowthGlobal0X128, the all-time global fee growth, per unit of liquidity, in token0
+// Accepts feeGrowthGlobal1X128, the all-time global fee growth, per unit of liquidity, in token1
+// Accepts secondsPerLiquidityCumulativeX128, the all-time seconds per max(1, liquidity) of the pool
+// Accepts tickCumulative, the tick * time elapsed since the pool was first initialized
+// Accepts time, the current block timestamp cast to a uint32
+// Accepts upper, a boolean that is true for updating a position's upper tick, or false for updating a position's lower tick
+// Accepts maxLiquidity, the maximum liquidity allocation for a single tick
+// Returns flipped, a boolean that indicates whether the tick was flipped from initialized to uninitialized, or vice versa
+func (t *Ticks) update(
+	tick,
+	tickCurrent,
+	time int,
+	liquidityDelta,
+	feeGrowthGlobal0X128,
+	feeGrowthGlobal1X128,
+	secondsPerLiquidityCumulativeX128,
+	tickCumulative,
+	maxLiquidity *big.Int,
+	upper bool,
+) bool {
+	info := t.TickData[tick]
+
+	liquidityGrossBefore := info.LiquidityGross
+	liquidityGrossAfter := liquidityMath.AddDelta(liquidityGrossBefore, liquidityDelta)
+
+	if liquidityGrossAfter.Cmp(maxLiquidity) == 1 {
+		panic("TICK: LO")
+	}
+
+	flipped := (liquidityGrossAfter.Cmp(big.NewInt(0)) == 1) != (liquidityGrossBefore.Cmp(big.NewInt(0)) == 1)
+
+	if liquidityGrossBefore.Cmp(big.NewInt(0)) == 1 {
+		// By convention, Uniswap assumes that all growth before a tick was initialized happened _below_ the tick
+		if tick <= tickCurrent {
+			info.FeeGrowthOutside0X128 = feeGrowthGlobal0X128
+			info.FeeGrowthOutside1X128 = feeGrowthGlobal1X128
+			info.SecondsPerLiquidityOutsideX128 = secondsPerLiquidityCumulativeX128
+			info.TickCumulativeOutside = tickCumulative
+			info.SecondsOutside = time
+		}
+		info.Initialized = true
+	}
+
+	info.LiquidityGross = liquidityGrossAfter
+
+	if upper {
+		info.LiquidityNet.Sub(info.LiquidityNet, liquidityDelta)
+	} else {
+		info.LiquidityNet.Add(info.LiquidityNet, liquidityDelta)
+	}
+
+	return flipped
 }
 
 func Init() *Ticks {
