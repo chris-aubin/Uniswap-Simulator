e7c954a6481ac408710a0494264cd61365ba9d30
Chris Aubin <69503979+chris-aubin@users.noreply.github.com>
2023-03-30 16:38:17 -0400

    Move constants to dedicated package

--- src/libraries/tickmath/tickMath.go
+++ src/libraries/tickmath/tickMath.go
@@ -2,46 +2,8 @@ package tickMath
 
 import (
 	"math/big"
-)
 
-const (
-	MinTick = -887272
-	// The maximum tick that may be passed to #getSqrtRatioAtTick computed from log base 1.0001 of 2**128
-	MaxTick = 887272
-	// The minimum value that can be returned from #getSqrtRatioAtTick. Equivalent to getSqrtRatioAtTick(MIN_TICK)
-	MinSqrtRatio = 4295128739
-)
-
-var (
-	// The minimum tick that may be passed to #getSqrtRatioAtTick computed from log base 1.0001 of 2**-128
-	MinTickBig = big.NewInt(-887272)
-	// The maximum tick that may be passed to #getSqrtRatioAtTick computed from log base 1.0001 of 2**128
-	MaxTickBig = big.NewInt(887272)
-	// The minimum value that can be returned from #getSqrtRatioAtTick. Equivalent to getSqrtRatioAtTick(MIN_TICK)
-	MinSqrtRatioBig = big.NewInt(4295128739)
-	// The maximum value that can be returned from #getSqrtRatioAtTick. Equivalent to getSqrtRatioAtTick(MAX_TICK)
-	MaxSqrtRatio, _ = new(big.Int).SetString("1461446703485210103287273052203988822378723970342", 10)
-	// The maximum 256 bit unsigned integer
-	MaxUint256, _ = new(big.Int).SetString("0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff", 16)
-	MaxUint128, _ = new(big.Int).SetString("0xffffffffffffffffffffffffffffffff", 16)
-	MaxUint64, _  = new(big.Int).SetString("0xffffffffffffffff", 16)
-	MaxUint32, _  = new(big.Int).SetString("0xffffffff", 16)
-	MaxUint16, _  = new(big.Int).SetString("0xffff", 16)
-	MaxUint8, _   = new(big.Int).SetString("0xff", 16)
-	MaxUint4, _   = new(big.Int).SetString("0xf", 16)
-	MaxUint2, _   = new(big.Int).SetString("0x3", 16)
-	MaxUint1, _   = new(big.Int).SetString("0x1", 16)
-	MaxUints      = map[int]*big.Int{
-		8: MaxUint256,
-		7: MaxUint128,
-		6: MaxUint64,
-		5: MaxUint32,
-		4: MaxUint16,
-		3: MaxUint8,
-		2: MaxUint4,
-		1: MaxUint2,
-		0: MaxUint1,
-	}
+	"github.com/chris-aubin/Uniswap-Simulator/src/libraries/constants"
 )
 
 // Computes  the sqrt price for ticks of size 1.0001, i.e. sqrt(1.0001^tick)
@@ -52,12 +14,14 @@ func getSqrtRatioAtTick(tick int) *big.Int {
 		absTick = -tick
 	}
 
-	if absTick > MaxTick {
+	if absTick > constants.MaxTick {
 		panic("tickMath.getSqrtRatioAtTick: INVALID_TICK")
 	}
 
+	// The tick is processed one bit at a time (0x1 = 1, 0x2 = 10, 0x4 = 100, etc.)
 	ratio := new(big.Int)
 	if absTick&0x1 != 0 {
+		// 0xfffcb933bd6fad37aa2d162d1a594001 = 340265354078544963557816517032075149313
 		ratio.SetString("0xfffcb933bd6fad37aa2d162d1a594001", 16)
 	} else {
 		ratio.SetString("0x100000000000000000000000000000000", 16)
@@ -120,7 +84,7 @@ func getSqrtRatioAtTick(tick int) *big.Int {
 		ratio = mulShift(ratio, "0x48a170391f7dc42444e8fa2")
 	}
 	if tick > 0 {
-		ratio = new(big.Int).Div(MaxUint256, ratio)
+		ratio = new(big.Int).Div(constants.MaxUint256, ratio)
 	}
 
 	// This divides by 1<<32
@@ -138,7 +102,7 @@ func getSqrtRatioAtTick(tick int) *big.Int {
 // Accepts sqrtPriceX96, the sqrt ratio for which to compute the tick
 // Returns the greatest tick for which the ratio is less than or equal to the input ratio
 func getTickAtSqrtRatio(sqrtPriceX96 *big.Int) *big.Int {
-	if (sqrtPriceX96.Cmp(MaxSqrtRatio) != -1) || (sqrtPriceX96.Cmp(MinSqrtRatioBig) != 1) {
+	if (sqrtPriceX96.Cmp(constants.MaxSqrtRatio) != -1) || (sqrtPriceX96.Cmp(constants.MinSqrtRatioBig) != 1) {
 		panic("tickMath.getTickAtSqrtRatio: INVALID_SQRT_RATIO")
 	}
 
@@ -147,7 +111,7 @@ func getTickAtSqrtRatio(sqrtPriceX96 *big.Int) *big.Int {
 	r := new(big.Int).Lsh(sqrtPriceX96, 32)
 	msb := big.NewInt(0)
 	for i := 7; i > 0; i-- {
-		cmp := ratio.Cmp(MaxUints[i])
+		cmp := ratio.Cmp(constants.MaxUints[i])
 		if cmp == -1 {
 			cmp = 0
 		}
@@ -155,7 +119,7 @@ func getTickAtSqrtRatio(sqrtPriceX96 *big.Int) *big.Int {
 		msb = new(big.Int).Or(msb, f)
 		r = new(big.Int).Rsh(r, uint(f.Int64()))
 	}
-	cmp := ratio.Cmp(MaxUints[0])
+	cmp := ratio.Cmp(constants.MaxUints[0])
 	if cmp == -1 {
 		cmp = 0
 	}
