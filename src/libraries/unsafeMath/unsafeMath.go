0725927eb11468f63207bb6190293cc6e6afdca0
Chris Aubin <69503979+chris-aubin@users.noreply.github.com>
2023-04-06 23:13:47 -0400

    Added simulation of overflow to unsafeMath.go

--- src/libraries/unsafeMath/unsafeMath.go
+++ src/libraries/unsafeMath/unsafeMath.go
@@ -1,17 +1,17 @@
-package unsafeMath
-
+// Package unsafeMath simulates the Uniswap unsafeMath library.
+//
 // In production Uniswap uses these functions that do not check inputs or
-// outputs only when overflow is unavoidable. Need to adjust these functions to
-// simulate overflow, because when using the Go big.Int library, overflow is
-// not an issue.
+// outputs only when overflow is unavoidable. These functions simulate overflow,
+// because when using the Go big.Int library, overflow is not an issue.
+package unsafeMath
 
 import (
 	"math/big"
+
+	"github.com/chris-aubin/Uniswap-Simulator/src/libraries/constants"
 )
 
 // Calculates ceil(numerator/denominator) for *big.Ints
-// numerator is the numerator
-// denominator is the denominator
 func DivRoundingUp(
 	numerator,
 	denominator *big.Int,
@@ -20,6 +20,12 @@ func DivRoundingUp(
 	quotient, remainder := new(big.Int).DivMod(numerator, denominator, remainder)
 	if remainder.Cmp(big.NewInt(0)) == 1 {
 		quotient.Add(quotient, big.NewInt(1))
+	}
+
+	// Check whether result could fit in a uint256, if not try to simulate
+	// overflow.
+	if quotient.Cmp(constants.MaxUint256) >= 1 {
+		return new(big.Int).Mod(quotient, constants.MaxUint256)
 	}
 	return quotient
 }
