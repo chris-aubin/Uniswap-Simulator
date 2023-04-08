7f23d0cebe7acf8b6944bd336da57d6acae768c6
Chris Aubin <69503979+chris-aubin@users.noreply.github.com>
2023-04-08 16:53:50 -0400

    GetSqrtRatioAtTick passes tests

--- src/libraries/tickmath/tickMath.go
+++ src/libraries/tickmath/tickMath.go
@@ -1,6 +1,7 @@
 package tickMath
 
 import (
+	"fmt"
 	"math/big"
 
 	"github.com/chris-aubin/Uniswap-Simulator/src/libraries/constants"
@@ -8,33 +9,15 @@ import (
 
 // Computes  the sqrt price for ticks of size 1.0001, i.e. sqrt(1.0001^tick)
 // Supports prices between 2**-128 and 2**128
-func getSqrtRatioAtTick(tick int) *big.Int {
-
-	// price := big.NewFloat(1.0001)
-	// absTick := tick
-	// if tick < 0 {
-	// 	absTick = -tick
-	// }
-	// if absTick == 0 {
-	// 	sqrtPrice = big.NewFloat(1)
-	// } else if absTick == 1 {
-	// 	sqrtPrice = new(big.Float).Sqrt(price)
-	// } else{
-	// 	for i := 1; i < absTick; i++ {
-	// 		price = new(big.Float).Mul(price, big.NewFloat(1.0001))
-	// 	}
-	// }
-
-	// if tick < 0 {
-	// 	sqrtPrice = new(big.Float).Quo(big.NewFloat(1), sqrtPrice)
-	// }
-
+func GetSqrtRatioAtTick(tick int) *big.Int {
 	absTick := tick
 	if tick < 0 {
 		absTick = -tick
 	}
 
 	if absTick > constants.MaxTick {
+		fmt.Println(absTick)
+		fmt.Println(constants.MaxTick)
 		panic("tickMath.getSqrtRatioAtTick: INVALID_TICK")
 	}
 
@@ -42,77 +25,79 @@ func getSqrtRatioAtTick(tick int) *big.Int {
 	// Each of these magic values is sqrt(1/1.0001)^2**bit, i.e. the square root
 	// of the price ratio. The magic values are used to compute
 	// sqrt(1.0001^tick) by using the fact that x^c = x^a*x^b where a + b = c.
+	// ratio := new(big.Float)
 	ratio := new(big.Int)
 	if absTick&0x1 != 0 {
-		// 0xfffcb933bd6fad37aa2d162d1a594001 == qrt(1/1.0001)^1
+		// 0xfffcb933bd6fad37aa2d162d1a594001 == sqrt(1/1.0001)^1
 		// Can check by comparing
 		// int('0xfffcb933bd6fad37aa2d162d1a594001', 16)/2**128
 		// to
 		// math.sqrt(1/1.0001)**0
-		ratio.SetString("0xfffcb933bd6fad37aa2d162d1a594001", 16)
+
+		ratio.SetString("fffcb933bd6fad37aa2d162d1a594001", 16)
 	} else {
 		// 0x100000000000000000000000000000000 ==sqrt(1/1.0001)^0
-		ratio.SetString("0x100000000000000000000000000000000", 16)
+		ratio.SetString("100000000000000000000000000000000", 16)
 	}
 	if (absTick & 0x2) != 0 {
 		// 0xfff97272373d413259a46990580e213a == sqrt(1/1.0001)^2
-		ratio = mulShift(ratio, "0xfff97272373d413259a46990580e213a")
+		ratio = mulShift(ratio, "fff97272373d413259a46990580e213a")
 	}
 	if (absTick & 0x4) != 0 {
 		// 0xfff2e50f5f656932ef12357cf3c7fdcc == sqrt(1/1.0001)^4
-		ratio = mulShift(ratio, "0xfff2e50f5f656932ef12357cf3c7fdcc")
+		ratio = mulShift(ratio, "fff2e50f5f656932ef12357cf3c7fdcc")
 	}
 	if (absTick & 0x8) != 0 {
 		// 0xfff2e50f5f656932ef12357cf3c7fdcc == sqrt(1/1.0001)^4
-		ratio = mulShift(ratio, "0xffe5caca7e10e4e61c3624eaa0941cd0")
+		ratio = mulShift(ratio, "ffe5caca7e10e4e61c3624eaa0941cd0")
 	}
 	if (absTick & 0x10) != 0 {
-		ratio = mulShift(ratio, "0xffcb9843d60f6159c9db58835c926644")
+		ratio = mulShift(ratio, "ffcb9843d60f6159c9db58835c926644")
 	}
 	if (absTick & 0x20) != 0 {
-		ratio = mulShift(ratio, "0xff973b41fa98c081472e6896dfb254c0")
+		ratio = mulShift(ratio, "ff973b41fa98c081472e6896dfb254c0")
 	}
 	if (absTick & 0x40) != 0 {
-		ratio = mulShift(ratio, "0xff2ea16466c96a3843ec78b326b52861")
+		ratio = mulShift(ratio, "ff2ea16466c96a3843ec78b326b52861")
 	}
 	if (absTick & 0x80) != 0 {
-		ratio = mulShift(ratio, "0xfe5dee046a99a2a811c461f1969c3053")
+		ratio = mulShift(ratio, "fe5dee046a99a2a811c461f1969c3053")
 	}
 	if (absTick & 0x100) != 0 {
-		ratio = mulShift(ratio, "0xfcbe86c7900a88aedcffc83b479aa3a4")
+		ratio = mulShift(ratio, "fcbe86c7900a88aedcffc83b479aa3a4")
 	}
 	if (absTick & 0x200) != 0 {
-		ratio = mulShift(ratio, "0xf987a7253ac413176f2b074cf7815e54")
+		ratio = mulShift(ratio, "f987a7253ac413176f2b074cf7815e54")
 	}
 	if (absTick & 0x400) != 0 {
-		ratio = mulShift(ratio, "0xf3392b0822b70005940c7a398e4b70f3")
+		ratio = mulShift(ratio, "f3392b0822b70005940c7a398e4b70f3")
 	}
 	if (absTick & 0x800) != 0 {
-		ratio = mulShift(ratio, "0xe7159475a2c29b7443b29c7fa6e889d9")
+		ratio = mulShift(ratio, "e7159475a2c29b7443b29c7fa6e889d9")
 	}
 	if (absTick & 0x1000) != 0 {
-		ratio = mulShift(ratio, "0xd097f3bdfd2022b8845ad8f792aa5825")
+		ratio = mulShift(ratio, "d097f3bdfd2022b8845ad8f792aa5825")
 	}
 	if (absTick & 0x2000) != 0 {
-		ratio = mulShift(ratio, "0xa9f746462d870fdf8a65dc1f90e061e5")
+		ratio = mulShift(ratio, "a9f746462d870fdf8a65dc1f90e061e5")
 	}
 	if (absTick & 0x4000) != 0 {
-		ratio = mulShift(ratio, "0x70d869a156d2a1b890bb3df62baf32f7")
+		ratio = mulShift(ratio, "70d869a156d2a1b890bb3df62baf32f7")
 	}
 	if (absTick & 0x8000) != 0 {
-		ratio = mulShift(ratio, "0x31be135f97d08fd981231505542fcfa6")
+		ratio = mulShift(ratio, "31be135f97d08fd981231505542fcfa6")
 	}
 	if (absTick & 0x10000) != 0 {
-		ratio = mulShift(ratio, "0x9aa508b5b7a84e1c677de54f3e99bc9")
+		ratio = mulShift(ratio, "9aa508b5b7a84e1c677de54f3e99bc9")
 	}
 	if (absTick & 0x20000) != 0 {
-		ratio = mulShift(ratio, "0x5d6af8dedb81196699c329225ee604")
+		ratio = mulShift(ratio, "5d6af8dedb81196699c329225ee604")
 	}
 	if (absTick & 0x40000) != 0 {
-		ratio = mulShift(ratio, "0x2216e584f5fa1ea926041bedfe98")
+		ratio = mulShift(ratio, "2216e584f5fa1ea926041bedfe98")
 	}
 	if (absTick & 0x80000) != 0 {
-		ratio = mulShift(ratio, "0x48a170391f7dc42444e8fa2")
+		ratio = mulShift(ratio, "48a170391f7dc42444e8fa2")
 	}
 	// Because each of these magic values is sqrt(1/1.0001)^2**bit, if tick > 0
 	// we need to invert the ratio (calculate 1/ratio). When using Q128.128
@@ -121,14 +106,15 @@ func getSqrtRatioAtTick(tick int) *big.Int {
 		ratio = new(big.Int).Div(constants.MaxUint256, ratio)
 	}
 
+	// return ratio
 	// This divides by 1<<32 to convert from Q128.128 to Q64.96
 	// We then downcast because we know the result always fits within 160 bits due to our tick input constraint
 	// We round up in the division so getTickAtSqrtRatio of the output price is always consistent
 	rounding := big.NewInt(0)
-	if ratio.Mod(ratio, big.NewInt(1<<32)).Cmp(big.NewInt(0)) != 0 {
+	if new(big.Int).Mod(ratio, big.NewInt(1<<32)).Cmp(big.NewInt(0)) != 0 {
 		rounding = big.NewInt(1)
 	}
-	sqrtPriceX96 := ratio.Rsh(ratio, 32).Add(ratio, rounding)
+	sqrtPriceX96 := new(big.Int).Add(new(big.Int).Rsh(ratio, 32), rounding)
 	return sqrtPriceX96
 }
 
