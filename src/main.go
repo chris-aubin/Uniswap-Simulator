e6a0f5e53804aa11ad7a9f437a6c3b9cde341382
Chris Aubin <69503979+chris-aubin@users.noreply.github.com>
2023-04-12 22:13:57 -0400

    Read in paths from command line

--- src/main.go
+++ src/main.go
@@ -5,21 +5,23 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/chris-aubin/Uniswap-Simulator/src/libraries/pool"
+	"github.com/chris-aubin/Uniswap-Simulator/src/libraries/simulation"
 	"github.com/chris-aubin/Uniswap-Simulator/src/libraries/transaction"
 )
 
-func getTransactions(transactionsRaw []byte) *[]transaction.Transaction {
+func getTransactions(transactionsRaw []byte) []*transaction.Transaction {
 	type getTransactionsInput struct {
-		Data []transaction.Transaction
+		Data []*transaction.Transaction
 	}
 	var transactionsInput getTransactionsInput
-	var transactions []transaction.Transaction
+	var transactions []*transaction.Transaction
 
 	json.Unmarshal(transactionsRaw, &transactionsInput)
 	transactions = transactionsInput.Data
-	return &transactions
+	return transactions
 }
 
 func getPoolState(poolRaw []byte) *pool.Pool {
@@ -47,37 +49,53 @@ func getGasAvs(gasAvsRaw []byte) *map[string]float64 {
 }
 
 func main() {
-	pathToTransactions := flag.String("transactions", "./data/transactions.txt", "Path to file containing transactions for simulation")
-	pathToPoolState := flag.String("pool", "./data/poolState.txt", "Path to file containing pool state for simulation")
-	pathToGasAvs := flag.String("gasAvs", "./data/gasAvs.txt", "Path to file containing gas averages for simulation")
+	relPathToTransactions := flag.String("transactions", "../data/transactions.txt", "Path to file containing transactions for simulation")
+	relPathToPoolState := flag.String("pool", "../data/poolState.txt", "Path to file containing pool state for simulation")
+	// relPathToGasAvs := flag.String("gasAvs", "../data/gasAvs.txt", "Path to file containing gas averages for simulation")
 	flag.Parse()
 
-	transactionsRaw, err := os.ReadFile(*pathToTransactions)
+	absPathToTransactions, err := filepath.Abs(*relPathToTransactions)
 	if err != nil {
-		message := fmt.Sprintf("Error reading transactions file at path: %s", *pathToTransactions)
+		message := fmt.Sprint("Failed to get absolute path to file containing transactions:", err)
 		panic(message)
 	}
 
-	poolStateRaw, err := os.ReadFile(*pathToPoolState)
+	absPathToPoolState, err := filepath.Abs(*relPathToPoolState)
 	if err != nil {
-		message := fmt.Sprintf("Error reading pool state file at path: %s", *pathToPoolState)
+		message := fmt.Sprint("Failed to get absolute path to file containing pool state:", err)
 		panic(message)
 	}
 
-	gasAvsRaw, err := os.ReadFile(*pathToGasAvs)
+	// absPathToGasAvs, err := filepath.Abs(*relPathToGasAvs)
+	// if err != nil {
+	// 	message := fmt.Sprint("Failed to get absolute path to file containing gas averages:", err)
+	// 	panic(message)
+	// }
+
+	transactionsRaw, err := os.ReadFile(absPathToTransactions)
 	if err != nil {
-		message := fmt.Sprintf("Error reading gas averages file at path: %s", *pathToGasAvs)
+		message := fmt.Sprintf("Error reading transactions file at path (relative path, absolute path): %s, %s, %v", *relPathToTransactions, absPathToTransactions, err)
 		panic(message)
 	}
+
+	poolStateRaw, err := os.ReadFile(absPathToPoolState)
+	if err != nil {
+		message := fmt.Sprintf("Error reading pool state file at path (relative path, absolute path): %s, %s, %v", *relPathToPoolState, absPathToPoolState, err)
+		panic(message)
+	}
+
+	// gasAvsRaw, err := os.ReadFile(absPathToGasAvs)
+	// if err != nil {
+	// 	message := fmt.Sprintf("Error reading gas averages file at path (relative path, absolute path): %s, %s, %v", *relPathToGasAvs, absPathToGasAvs, err)
+	// 	panic(message)
+	// }
 
 	transactions := getTransactions(transactionsRaw)
 	poolState := getPoolState(poolStateRaw)
-	gasAvs := getGasAvs(gasAvsRaw)
+	// gasAvs := getGasAvs(gasAvsRaw)
 
-	fmt.Println(transactions)
-	fmt.Println()
-	fmt.Println(poolState)
-	fmt.Println()
-	fmt.Println(gasAvs)
-	fmt.Println()
+	s := simulation.Make(poolState, transactions)
+	s.Simulate()
+	fmt.Println("Pool state after simulation:")
+	fmt.Printf("%+v", s.Pool)
 }
