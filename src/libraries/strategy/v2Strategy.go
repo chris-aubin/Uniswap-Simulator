a1fec525c6fbcc14321db26196a065ac690b1248
Chris Aubin <69503979+chris-aubin@users.noreply.github.com>
2023-04-15 12:40:39 -0400

    Work on v2Strategy and restructure strategy folder

--- src/libraries/strategy/v2Strategy.go
+++ src/libraries/strategy/v2Strategy.go
@@ -6,24 +6,20 @@ import (
 	"github.com/chris-aubin/Uniswap-Simulator/src/libraries/constants"
 	"github.com/chris-aubin/Uniswap-Simulator/src/libraries/pool"
 	"github.com/chris-aubin/Uniswap-Simulator/src/libraries/tickMath"
+	"github.com/chris-aubin/Uniswap-Simulator/src/libraries/utilities"
 )
 
 type V2Strategy struct {
-	Address   string
-	Amount0   *big.Int
-	Amount1   *big.Int
-	Positions []*StrategyPosition
+	Address      string
+	Amount0      *big.Int
+	Amount1      *big.Int
+	GasAllowance *big.Int
+	GasAvs       *GasAvs
+	Positions    []*StrategyPosition
 }
 
 func (s *V2Strategy) Rebalance(p *pool.Pool) {
-	s.Amount0, s.Amount1 = s.BurnAll(p)
-
-	s.mintPosition(constants.MinTick, constants.MaxTick)
 	return
-}
-
-func NewV2Strategy(amount0, amount1 *big.Int) *V2Strategy {
-	return new(V2Strategy)
 }
 
 func (s *V2Strategy) BurnAll(p *pool.Pool) (amount0, amount1 *big.Int) {
@@ -40,32 +36,35 @@ func (s *V2Strategy) BurnAll(p *pool.Pool) (amount0, amount1 *big.Int) {
 	return
 }
 
-func (s *V2Strategy) mintPosition(tickLower, tickUpper int, p *pool.Pool) {
+func (s *V2Strategy) mintPosition(p *pool.Pool) {
+	tickLower := constants.MinTick
+	tickUpper := constants.MaxTick
 	sqrtRatioAX96 := tickMath.GetSqrtRatioAtTick(tickLower)
 	sqrtRatioBX96 := tickMath.GetSqrtRatioAtTick(tickUpper)
 
-	// amount := la.GetLiquidityForAmount(s.Pool.SqrtRatioX96, sqrtRatioAX96, sqrtRatioBX96, s.Amount0, s.Amount1)
-	// if amount.IsZero() {
-	// 	return
-	// }
-	// s.Positions = append(s.Positions, StrategyPosition{
-	// 	amount:    amount,
-	// 	tickLower: tickLower,
-	// 	tickUpper: tickUpper,
-	// })
+	amount := utilities.GetLiquidityForAmount(p.Slot0.SqrtPriceX96, sqrtRatioAX96, sqrtRatioBX96, s.Amount0, s.Amount1)
+	if amount.Cmp(big.NewInt(0)) <= 0 {
+		return
+	}
+	s.Positions = append(s.Positions, &StrategyPosition{
+		TickLower: tickLower,
+		TickUpper: tickUpper,
+		Amount:    amount,
+	})
 
 	// amount0, amount1 := s.Pool.MintStrategy(tickLower, tickUpper, amount)
 	// s.Amount0.Sub(s.Amount0, amount0)
 	// s.Amount1.Sub(s.Amount1, amount1)
 }
 
-func (s *V2Strategy) Init() (amount0, amount1 *big.Int) {
+func Make(amount0, amount1 *big.Int, p *pool.Pool, g *GasAvs) {
+	s := new(V2Strategy)
+	s.Address = "0x0000000000000000000000000000000000000001"
 	s.Amount1 = new(big.Int).Set(amount0)
 	s.Amount1 = new(big.Int).Set(amount1)
+	s.GasAvs = g
+	s.Positions = make([]*StrategyPosition, 0)
 
-	// New Positions
-	tickLower := -887270
-	tickUpper := -tickLower
-	// s.mintPosition(tickLower, tickUpper)
+	s.mintPosition(p)
 	return
 }
