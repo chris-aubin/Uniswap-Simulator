989de1854d6355be5fd7f3fd1a6ecc5e7a2c9eda
Chris Aubin <69503979+chris-aubin@users.noreply.github.com>
2023-04-15 09:48:55 -0400

    Started working on strategy package

--- src/libraries/strategy/strategy.go
+++ src/libraries/strategy/strategy.go
@@ -1,1 +1,35 @@
 package strategy
+
+import (
+	"math/big"
+
+	"github.com/chris-aubin/Uniswap-Simulator/src/libraries/pool"
+	"github.com/chris-aubin/Uniswap-Simulator/src/libraries/simulation"
+)
+
+// StrategyPosition represents a position held by a strategy.
+// Positions are indexed using the owner's address, tickLower, and tickUpper.
+type StrategyPosition struct {
+	TickLower int
+	TickUpper int
+	Amount    *big.Int
+}
+
+type Strategy struct {
+	// Address of the strategy
+	Address string
+	// Amount of token0 in the strategy
+	Amount0 *big.Int
+	// Amount of token1 in the strategy
+	Amount1 *big.Int
+	// Positions held by the strategy
+	Positions []*StrategyPosition
+	// Average gas price for mints, burns and swaps
+	GasAvs *simulation.GasAvs
+	// Initialize the strategy with the given amounts
+	Init func(*big.Int, *big.Int, *pool.Pool, *simulation.GasAvs) *Strategy
+	// Adjust strategy positions positions (called at update interval)
+	Rebalance func(*pool.Pool)
+	// Burn all positions and collect all tokens
+	BurnAll func(*pool.Pool) (*big.Int, *big.Int)
+}
