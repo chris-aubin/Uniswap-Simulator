146b29eba01403c5fe03cbef74e8826321a552f4
Chris Aubin <69503979+chris-aubin@users.noreply.github.com>
2023-04-16 22:38:53 -0400

    Update simulation.go

--- src/libraries/simulation/simulation.go
+++ src/libraries/simulation/simulation.go
@@ -12,35 +12,38 @@ type Simulation struct {
 	Strategy     *strategy.Strategy
 	Pool         *pool.Pool
 	Transactions []transaction.Transaction
-	StartBlock   int
-	EndBlock     int
+	// StartBlock   int
+	// EndBlock     int
 }
 
-func Make(pool *pool.Pool, transactions []transaction.Transaction, strategy *strategy.Strategy, startBlock, endBlock, updateInterval int) *Simulation {
+// func Make(pool *pool.Pool, transactions []transaction.Transaction, strategy *strategy.Strategy, startBlock, endBlock, updateInterval int) *Simulation {
+func Make(pool *pool.Pool, transactions []transaction.Transaction, strategy *strategy.Strategy) *Simulation {
 	return &Simulation{
 		Strategy:     strategy,
 		Pool:         pool,
 		Transactions: transactions,
-		StartBlock:   startBlock,
-		EndBlock:     endBlock,
+		// StartBlock:   startBlock,
+		// EndBlock:     endBlock,
 	}
 }
 
 func (s *Simulation) Simulate() {
-	for _, t := range s.Transactions {
+	s.Strategy.Init(s.Pool)
+	for i, t := range s.Transactions {
 
-		if t.BlockNo < s.StartBlock {
-			continue
-		}
+		// if t.BlockNo < s.StartBlock {
+		// 	continue
+		// }
 
-		if t.BlockNo > s.EndBlock {
-			// End simulation (must calculate returns, etc)
-			break
-		}
+		// if t.BlockNo > s.EndBlock {
+		// End simulation (must calculate returns, etc)
+		// 	break
+		// }
 
-		if (t.BlockNo-s.StartBlock)%s.Strategy.UpdateInterval == 0 {
+		// if (t.BlockNo-s.StartBlock)%s.Strategy.UpdateInterval == 0 {
+		if i%s.Strategy.UpdateInterval == 0 {
 			// Call rebalance function
-			s.Strategy.Rebalance(s.Pool)
+			s.Strategy.Rebalance(s.Pool, s.Strategy)
 		}
 
 		transaction.Execute(t, s.Pool)
