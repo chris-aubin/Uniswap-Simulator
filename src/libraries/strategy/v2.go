8bed9a541c28d41986f306ff24da432a2a1f8cf5
Chris Aubin <69503979+chris-aubin@users.noreply.github.com>
2023-04-18 11:05:52 -0400

    Update v2.go

--- src/libraries/strategy/v2.go
+++ src/libraries/strategy/v2.go
@@ -5,15 +5,14 @@ import (
 	"math/big"
 
 	"github.com/chris-aubin/Uniswap-Simulator/src/libraries/constants"
+	"github.com/chris-aubin/Uniswap-Simulator/src/libraries/liquidityAmounts"
 	"github.com/chris-aubin/Uniswap-Simulator/src/libraries/pool"
 	"github.com/chris-aubin/Uniswap-Simulator/src/libraries/tickMath"
-	"github.com/chris-aubin/Uniswap-Simulator/src/libraries/utilities"
 )
 
 func V2StrategyRebalance(p *pool.Pool, s *Strategy) {
 	fmt.Println("V2StrategyRebalance")
 	if len(s.Positions) == 0 {
-		fmt.Println("ABOUT TO MINT")
 		V2StrategyMintPosition(p, s)
 	}
 	return
@@ -26,16 +25,22 @@ func V2StrategyMintPosition(p *pool.Pool, s *Strategy) {
 	sqrtRatioAX96 := tickMath.GetSqrtRatioAtTick(tickLower)
 	sqrtRatioBX96 := tickMath.GetSqrtRatioAtTick(tickUpper)
 
-	amount := utilities.GetLiquidityForAmount(p.Slot0.SqrtPriceX96, sqrtRatioAX96, sqrtRatioBX96, s.Amount0, s.Amount1)
+	liquidity := liquidityAmounts.GetLiquidityForAmounts(p.Slot0.SqrtPriceX96, sqrtRatioAX96, sqrtRatioBX96, s.Amount0, s.Amount1)
+	amount0, amount1 := liquidityAmounts.GetAmountsForLiquidity(p.Slot0.SqrtPriceX96, sqrtRatioAX96, sqrtRatioBX96, liquidity)
 	fmt.Println("V2StrategyMintPosition")
-	fmt.Println("amount", amount)
-	if amount.Cmp(big.NewInt(0)) <= 0 {
+	fmt.Println("liquidity", liquidity)
+	fmt.Println("amount0", amount0)
+	fmt.Println("amount1", amount1)
+	if liquidity.Cmp(big.NewInt(0)) <= 0 {
 		return
 	}
+	amount0mint, amount1mint := p.Mint(s.Address, tickLower, tickUpper, liquidity)
+	fmt.Println("amount0mint", amount0mint)
+	fmt.Println("amount1mint", amount1mint)
 	s.Positions = append(s.Positions, &StrategyPosition{
 		TickLower: tickLower,
 		TickUpper: tickUpper,
-		Amount:    amount,
+		Liquidity: liquidity,
 	})
 
 }
